cc03a7b09fa2682804d0285971f4fc52b0641127
lfz <a188616786a>
2017-05-12 21:52:34 +0800

    add scheduler

--- tem_reserve.py
+++ tem_reserve.py
@@ -1,13 +1,36 @@
 # encoding:utf-8
 import json
+import os
 import urllib
 import urllib2
 import cookielib
+from time import sleep
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 INSTRUMENT_OLD_F20 = '28ad18ae3ebb4f91b1d52553019ca381'
 INSTRUMENT_NEW_F20 = '563e690aae7b41dfb6da1880f291e65b'
-id2instrument = {'28ad18ae3ebb4f91b1d52553019ca381': '老F20',
-                 '563e690aae7b41dfb6da1880f291e65b': '新F20'}
+id2instrument = {INSTRUMENT_OLD_F20: '老F20', INSTRUMENT_NEW_F20: '新F20'}
+
+# 以下需填写
+# --------------------------------------
+email = os.getenv("username")
+password = os.getenv("password")
+reserve_info = [
+    dict(
+        reserveDate='',  # '2017年01月01日'
+        reserveStartTime='',  # '12:00'
+        reserveEndTime='',  # '13:00'
+        instrumentId=INSTRUMENT_NEW_F20  # INSTRUMENT_OLD_F20
+    )
+]
+start_time = dict(
+    day_of_week='sat',
+    hour=0,
+    minute=0,
+    second=0
+)
+# --------------------------------------
 
 
 class ReserveTem(object):
@@ -81,33 +104,36 @@ class ReserveTem(object):
             print '删除成功！'
 
 
-if __name__ == '__main__':
-    # 以下需填写
-    # --------------------------------------
-    email = ''
-    password = ''
-    reserve_info = [
-        dict(
-            reserveDate='2017年05月13日',  # '2017年05月14日'
-            reserveStartTime='12:00',  # '12:00'
-            reserveEndTime='13:00',  # '13:00'
-            instrumentId=INSTRUMENT_NEW_F20  # INSTRUMENT_OLD_F20
-        )
-    ]
-
-    # --------------------------------------
-
+def job():
     rsv = ReserveTem()
+    success_num = 0
 
-    # 登录
-    rsv.login(email, password)
+    while success_num < len(reserve_info):
+        # 登录
+        rsv.login(email, password)
 
-    # 预定
-    for info in reserve_info:
-        id = rsv.reserve(reserveDate=info['reserveDate'],
-                         reserveStartTime=info['reserveStartTime'],
-                         reserveEndTime=info['reserveEndTime'],
-                         instrumentId=info['instrumentId'])
-        # 填写预订信息
-        if id:
-            rsv.add_comment(instrumentId=info['instrumentId'], reserveRecordId=id, msg='f20')
+        # 预定
+        for info in reserve_info:
+            if info.get('success', False):
+                continue
+            id = rsv.reserve(reserveDate=info['reserveDate'],
+                             reserveStartTime=info['reserveStartTime'],
+                             reserveEndTime=info['reserveEndTime'],
+                             instrumentId=info['instrumentId'])
+            # 填写预订信息
+            if id:
+                info['success'] = True
+                success_num += 1
+                rsv.add_comment(instrumentId=info['instrumentId'], reserveRecordId=id, msg='f20')
+        sleep(1)
+
+if __name__ == '__main__':
+    try_login = ReserveTem()
+    if try_login.login(email, password):
+        scheduler = BlockingScheduler()
+        scheduler.add_job(job, 'cron', **start_time)
+        print 'job will start at : ' + start_time['day_of_week'].upper() + \
+              '. %02d:%02d:%02d' % (start_time['hour'], start_time['minute'], start_time['second'])
+        scheduler.start()
+    else:
+        print 'invalid username or password'
